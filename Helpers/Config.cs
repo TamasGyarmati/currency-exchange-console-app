93b12b6f920c6f846b5fc6ff50b1ea2a582c7d99
Gyarmati Tamás <tamasgy56@gmail.com>
2026-01-20 13:14:56 +0100

    Refactored from 60 minute to 1 day caching

--- Helpers/Config.cs
+++ Helpers/Config.cs
@@ -6,7 +6,7 @@ public static class Config
     public const string cacheFile = "currencyCache.json";
     public const string baseForCurrency = "USD";
     public const string outputType = "JSON";
-    public const int cacheDurationMinutes = 60;
+    public const int cacheDurationMinutes = 1440;
     public static string GetApiUrl() 
         => $"https://currencyapi.net/api/v1/rates?key={key}&base={baseForCurrency}&output={outputType}";
 }
