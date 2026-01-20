43e4458e57bdfcd77009700a56271b04a0fa68ae
Gyarmati Tamás <tamasgy56@gmail.com>
2026-01-20 14:16:13 +0100

    Exception handling, removing unnecessary access modifiers

--- Helpers/Config.cs
+++ Helpers/Config.cs
@@ -2,10 +2,11 @@ namespace CurrencyApp.Helpers;
 
 public static class Config
 {
-    public static readonly string? key = Environment.GetEnvironmentVariable("CURRENCY_API_KEY");
+    static readonly string key = Environment.GetEnvironmentVariable("CURRENCY_API_KEY")
+        ?? throw new InvalidOperationException("Environment variable 'CURRENCY_API_KEY' was not found");
     public const string cacheFile = "currencyCache.json";
-    public const string baseForCurrency = "USD";
-    public const string outputType = "JSON";
+    const string baseForCurrency = "USD";
+    const string outputType = "JSON";
     public const int cacheDurationMinutes = 1440;
     public static string GetApiUrl() 
         => $"https://currencyapi.net/api/v1/rates?key={key}&base={baseForCurrency}&output={outputType}";
